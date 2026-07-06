f844b20d53ea0268f4dc410207eefa7ca81c7f82
DEVKAILESH6 <devkaileshss@gmail.com>
2026-07-06 22:25:29 +0530

    Time: 453 ms (35.52%), Space: 0B (100%) - LeetHub - Time: O(, Space: O(?)

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT author_id  as id FROM Views WHERE author_id=viewer_id ORDER BY id;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
