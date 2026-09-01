a6078f0fd8a6c617b1ed0a3851cd3c22b276016e
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-01 23:09:36 +0530

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 880511-taking-input-GFG/880511-taking-input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class GFG 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();;
+        int n= sc.nextInt();
+        float f=sc.nextFloat();
+        System.out.println(s);
+        System.out.println(n);
+        System.out.println((int)Math.floor(f));
+    }
+}   
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
