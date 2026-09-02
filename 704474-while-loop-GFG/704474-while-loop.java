6fad9e4e127a5ece40b34ca9193f7f759ede1052
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-02 23:36:01 +0530

    Added solution - Time: O(x), Space: O(1)

--- 704474-while-loop-GFG/704474-while-loop.java
+++ 704474-while-loop-GFG/704474-while-loop.java
@@ -1,12 +1,15 @@
-class Solution 
-{
-    public static void utility(int x)
-    {
-        while(x>=0)
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        while(x >= 0)
         {
             System.out.print(x+" ");
             x--;
-        }   
+        }
+        
     }
 }
 
