7a86d66f990ca0da4b12b22aaa25c5c4e86692fc
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-05 20:59:28 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 705370-function-with-arguments-GFG/705370-function-with-arguments.java
@@ -0,0 +1,14 @@
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should return a+b
+class Solution 
+{
+    public static int argumentFunction(int a , int b)
+    {
+        return a+b;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
