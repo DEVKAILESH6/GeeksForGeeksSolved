b4584a46ab5dff986aa90c6e259b4bb0015388c2
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-05 20:59:11 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878750-ap-term-GFG/878750-ap-term.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = a + (n-1) * d;
+        System.out.println(sum);
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
