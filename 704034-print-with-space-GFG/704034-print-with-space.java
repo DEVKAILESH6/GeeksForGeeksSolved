5b7075aa54e12aba6b856553ebf64a8c6b2367aa
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-10 23:40:44 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 704034-print-with-space-GFG/704034-print-with-space.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class GFG{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String s1=sc.next();
+        System.out.print(s+" "+s1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
