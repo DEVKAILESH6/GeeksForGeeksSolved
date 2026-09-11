151285d1714b803f30fa049ca95483429bf5aef9
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-11 19:04:00 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 703782-java-input-output-GFG/703782-java-input-output.java
@@ -0,0 +1,11 @@
+class Solution {
+    static void printIntString(String S, int N) 
+    {
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
