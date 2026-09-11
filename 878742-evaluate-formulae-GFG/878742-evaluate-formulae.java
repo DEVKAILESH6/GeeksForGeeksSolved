40e83d55e9f6f94430d029b7ef20f9f3800f020b
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-11 18:58:16 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878742-evaluate-formulae-GFG/878742-evaluate-formulae.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = (a+b)/c+d;
+        System.out.println(e);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
