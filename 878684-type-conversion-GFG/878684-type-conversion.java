e51e0480790e0e7b7ec01bb78f823a6d8e8409f0
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-13 23:29:24 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878684-type-conversion-GFG/878684-type-conversion.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class GFG 
+{
+    public static void main(String args[]) 
+    {
+        Scanner s = new Scanner(System.in);
+        double d = s.nextDouble();
+        int n=(int) (d);
+        System.out.println(n);
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
