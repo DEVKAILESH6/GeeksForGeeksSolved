9fd7f02c17865caf36732caa9e088fc31c8d5e38
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-14 22:56:17 +0530

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 704101-print-with-separator-GFG/704101-print-with-separator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        String b = s.nextLine();
+        String separator = s.nextLine();
+        System.out.println(a+separator+b);
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
