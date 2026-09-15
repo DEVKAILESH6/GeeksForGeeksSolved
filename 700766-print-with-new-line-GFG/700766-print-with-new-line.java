c219744420de8ba07bc16d3b55e90923117929c0
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-15 23:26:44 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 700766-print-with-new-line-GFG/700766-print-with-new-line.java
@@ -0,0 +1,13 @@
+class GFG 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
