b8f9eab1dce5bd8a7b7e98894a141ee034a3147e
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-18 18:47:53 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 705385-decrement-array-values-by-1-GFG/705385-decrement-array-values-by-1.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public static int[] decrementArray(int[] arr, int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = arr[i] - 1;
+        }
+        return arr;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
