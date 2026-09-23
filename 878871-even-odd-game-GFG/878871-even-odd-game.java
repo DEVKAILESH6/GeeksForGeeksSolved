76f8baaaf67715ac7e65be8109bda9f7d14edbf6
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-23 23:06:16 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878871-even-odd-game-GFG/878871-even-odd-game.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Friend");
+        }
+        else
+        {
+            System.out.println("You");
+        }
+
+
+        s.close();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
