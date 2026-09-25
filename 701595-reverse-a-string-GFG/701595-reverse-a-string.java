28a37f5cf089ae1cef2959c80efbf1635ed49c87
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-25 22:35:43 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701595-reverse-a-string-GFG/701595-reverse-a-string.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public static String reverseString(String s) 
+    {
+        StringBuilder rev=new StringBuilder(s);
+        rev.reverse();
+        return rev.toString();
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
