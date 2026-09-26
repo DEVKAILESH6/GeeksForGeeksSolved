f0c1407e51a9b5a7b997457da3f36eafc9b34bc5
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-26 22:16:35 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 700133-check-for-binary-string-GFG/700133-check-for-binary-string.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public boolean isBinary(String s) 
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!='0'&&s.charAt(i)!='1')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
