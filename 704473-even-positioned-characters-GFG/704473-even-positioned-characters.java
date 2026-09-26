22cec8ed4d4db7d32a31fb46dfac7ec2ca64dd19
DEV KAILESH S <devkaileshss@gmail.com>
2026-09-26 22:17:06 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704473-even-positioned-characters-GFG/704473-even-positioned-characters.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public static void utility(String s) 
+    {
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(s.charAt(i));
+            }
+        }
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
